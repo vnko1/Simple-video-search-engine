7e7aff5d6c318414126b85b3aefd74218d467ccc
Andrii Valenko <andreyvalenko@gmail.com>
2023-05-27 16:44:43 +0300

    eds

--- src/scripts/services/searchApi.js
+++ src/scripts/services/searchApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 const API_KEY = "AIzaSyC6JURxa2KjjTgeOgWyyhspeCO_3o31nUs";
 const CX = "93fb8775a0c4043c8";
-const SITE_SEARCH = "youtube.com";
+const SITE_SEARCH = "youtube.com/music";
 axios.defaults.baseURL = BASE_URL;
 // const SITE_SEARCH_FILTER = "SITE_SEARCH_FILTER_I";
 
@@ -11,11 +11,11 @@ async function searchData(query, index = 1) {
     params: {
       key: API_KEY,
       cx: CX,
-      q: `${query}+more:pagemap:total-views`,
-      //   start: index,
+      q: query,
+      sort: "viewCount",
       siteSearch: SITE_SEARCH,
+      //   start: index,
 
-      //   sort: "total-views:d",
       //   siteSearchFilter: SITE_SEARCH_FILTER,
     },
   });
