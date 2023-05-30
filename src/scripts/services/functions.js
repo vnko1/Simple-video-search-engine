364637ba40ee2b954692b8d3b65b884de234be8a
Andrii Valenko <andreyvalenko@gmail.com>
2023-05-30 17:14:55 +0300

    add search data logic

--- src/scripts/services/functions.js
+++ src/scripts/services/functions.js
@@ -1,7 +1,9 @@
 import { searchVideo } from "./Search";
+import { parse } from "tinyduration";
+import millify from "millify";
+import { nanoid } from "nanoid";
 
 function formatData(response) {
-  console.log(response);
   if (response.queries.nextPage)
     searchVideo.setNextPageIndex(response.queries.nextPage[0].startIndex);
   else searchVideo.setNextPageIndex(null);
@@ -17,21 +19,30 @@ function formatData(response) {
     formattedData = response?.items.map((el) => {
       let pagemap = null;
 
-      if (el.pagemap.videoobject)
+      if (el.pagemap.videoobject) {
+        const duration = parse(el.pagemap.videoobject[0].duration);
+        duration.minutes = duration.minutes.toString().padStart(2, "0");
+        duration.seconds = duration.seconds.toString().padStart(2, "0");
+        const interactioncount = millify(
+          el.pagemap.videoobject[0].interactioncount,
+          { lowercase: true }
+        );
         pagemap = {
-          desciption: el.pagemap.videoobject[0].desciption,
-          duration: el.pagemap.videoobject[0].duration,
-          interactioncount: el.pagemap.videoobject[0].interactioncount,
+          desciption: el.pagemap.videoobject[0].description,
+          duration,
+          interactioncount,
           name: el.pagemap.videoobject[0].name,
           url: el.pagemap.videoobject[0].url,
           thumbnailurl: el.pagemap.videoobject[0].thumbnailurl,
           embedurl: el.pagemap.videoobject[0].embedurl,
         };
+      }
 
       return {
         displayLink: el.displayLink,
         formattedUrl: el.formattedUrl,
         pagemap,
+        id: nanoid(),
       };
     });
 
